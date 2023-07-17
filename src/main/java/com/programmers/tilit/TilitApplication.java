e99c87c5b8ecfd6906ce7e5a101887b8f01fd77d
kmebin <blowhuiee@gmail.com>
2023-07-18 00:11:36 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/programmers/tilit/TilitApplication.java
@@ -0,0 +1,11 @@
+package com.programmers.tilit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TilitApplication.class, args);
+    }
+}
---
+++ src/test/java/com/programmers/tilit/TilitApplicationTests.java
@@ -0,0 +1,11 @@
+package com.programmers.tilit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilitApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
