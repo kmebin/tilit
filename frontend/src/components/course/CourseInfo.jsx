877fa5bf752ca4a1626099d712ed1781204f5c0f
kmebin <blowhuiee@gmail.com>
2023-07-24 00:10:13 +0900

    feat: 강의 상세 조회 뷰 수정

--- frontend/src/components/course/CourseInfo.jsx
+++ frontend/src/components/course/CourseInfo.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { getCourseDetail } from '../../apis/course';
 import { useParams } from 'react-router-dom';
@@ -7,7 +7,16 @@ import { useEffect, useState } from 'react';
 const CourseInfo = ({ onClickRegister, onClickDelete }) => {
   const userId = localStorage.getItem('id');
   const { courseId } = useParams();
-  const [courseDetail, setCourseDetail] = useState({});
+  const [courseDetail, setCourseDetail] = useState({
+    id: 0,
+    category: '',
+    name: '',
+    teacherId: 0,
+    teacherName: '',
+    description: '',
+    price: 0,
+    studentCount: 0,
+  });
   const { id, category, name, teacherId, teacherName, description, price, studentCount } = courseDetail;
 
   const handleRegister = () => {
@@ -31,69 +40,64 @@ const CourseInfo = ({ onClickRegister, onClickDelete }) => {
   }, [courseId]);
 
   return (
-    <Card>
-      <Row>
-        <h5>
-          <b>강의 정보</b>
-        </h5>
-        <hr />
-      </Row>
-      <Row>
-        <Col md={8} className='mt-4 d-flex flex-column align-items-start p-3 pt-0'>
-          <Container>
-            <Card>
-              <Row>
-                <Card>
-                  <Card.Header>{category}</Card.Header>
-                  <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                      강사: <strong>{teacherName}</strong>
-                    </Card.Text>
-                    <Card.Text>
-                      <span className='text-primary'>{studentCount}</span>명의 수강생
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Text>소개</Card.Text>
-                  <Card body>{description}</Card>
-                </Card>
-              </Row>
-            </Card>
-          </Container>
-        </Col>
-        <Col md={4} className='summary p-4'>
-          <Container>
-            <Row className='pt-2 pb-3'>
-              <Col>
-                <h5>{price ? price.toLocaleString('ko-KR') : price}원</h5>
-              </Col>
-            </Row>
-            {userId && Number(userId) === teacherId ? (
-              <Row className='mt-3'>
-                <Col>
-                  <Link to={`/courses/${id}/update`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <div className='d-grid'>
-                      <Button variant='primary'>수정하기</Button>
-                    </div>
-                  </Link>
-                </Col>
-                <Col>
-                  <div className='d-grid'>
-                    <Button variant='danger' onClick={handleDelete}>
-                      삭제하기
-                    </Button>
-                  </div>
-                </Col>
-              </Row>
-            ) : (
-              <div className='d-grid gap-2'>
-                <Button onClick={handleRegister}>수강신청하기</Button>
+    <Container style={{ maxWidth: '560px' }}>
+      <h4 className='mb-3 mt-5'>강의 정보</h4>
+      <Form>
+        <Form.Group controlId='category' className='mb-3'>
+          <Form.Label>카테고리</Form.Label>
+          <Form.Control type='text' value={category} readOnly />
+        </Form.Group>
+
+        <Form.Group controlId='name' className='mb-3'>
+          <Form.Label>강의명</Form.Label>
+          <Form.Control type='text' value={name} readOnly />
+        </Form.Group>
+
+        <Form.Group controlId='teacher' className='mb-3'>
+          <Form.Label>강사</Form.Label>
+          <Form.Control type='text' value={teacherName} readOnly />
+        </Form.Group>
+
+        <Form.Group controlId='description' className='mb-3'>
+          <Form.Label>소개</Form.Label>
+          <Form.Control as='textarea' rows={3} value={description} readOnly />
+        </Form.Group>
+
+        <Form.Group controlId='price' className='mb-3'>
+          <Form.Label>가격</Form.Label>
+          <Form.Control type='text' value={price.toLocaleString('ko-KR') + '원'} readOnly />
+        </Form.Group>
+
+        <Form.Group controlId='price' className='mb-3'>
+          <Form.Label>수강생</Form.Label>
+          <Form.Control type='text' value={studentCount.toLocaleString('ko-KR') + '명'} readOnly />
+        </Form.Group>
+
+        <hr className='my-4' />
+        {userId && Number(userId) === teacherId ? (
+          <Row className='mt-3'>
+            <Col>
+              <Link to={`/courses/${id}/update`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <div className='d-grid'>
+                  <Button variant='primary'>수정하기</Button>
+                </div>
+              </Link>
+            </Col>
+            <Col>
+              <div className='d-grid'>
+                <Button variant='danger' onClick={handleDelete}>
+                  삭제하기
+                </Button>
               </div>
-            )}
-          </Container>
-        </Col>
-      </Row>
-    </Card>
+            </Col>
+          </Row>
+        ) : (
+          <div className='d-grid gap-2'>
+            <Button onClick={handleRegister}>수강신청하기</Button>
+          </div>
+        )}
+      </Form>
+    </Container>
   );
 };
 
