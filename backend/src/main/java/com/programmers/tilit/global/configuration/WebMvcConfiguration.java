2d8ef31ed8d2ba5c161702bafd936c5b0f3261da
kmebin <blowhuiee@gmail.com>
2023-07-23 18:07:06 +0900

    feat: 쿠키 요청 허용하도록 CORS 설정 수정

--- backend/src/main/java/com/programmers/tilit/global/configuration/WebMvcConfiguration.java
+++ backend/src/main/java/com/programmers/tilit/global/configuration/WebMvcConfiguration.java
@@ -18,7 +18,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true);
     }
 
     @Override
